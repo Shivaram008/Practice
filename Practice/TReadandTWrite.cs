cfc5ea958c1fadcea70d297c894102c66f489960
Shivaram008 <ram.cheruku@cognine.com>
2021-06-01 11:21:57 +0530

    Added samples

---
+++ Practice/TReadandTWrite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Practice
+{
+	public class TReadandTWrite
+	{
+		static string path = @"C:\Users\Ram Cheruku\source\repos\Practice\Practice\file5.txt";
+		public static void Main()
+        {
+			if (File.Exists(path))
+				File.Delete(path);
+			using(TextWriter tw = File.CreateText(path))
+            {
+				tw.WriteLine("Hello world !");
+				tw.WriteLine("Welcome to files concept");
+            }
+			using(TextReader tr = File.OpenText(path))
+            {
+				string s;
+				while((s=tr.ReadLine())!=null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+			Console.ReadKey();
+        }
+	}
+}
