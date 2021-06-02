a664f3ffd3ffef6fccf58ba10832467efd92e95e
Shivaram008 <ram.cheruku@cognine.com>
2021-06-02 16:24:00 +0530

    Events

---
+++ Practice/Event1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public delegate string MyDel(string str);
+    class Event1
+    {
+        event MyDel MyEvent;
+
+        public Event1()
+        {
+            this.MyEvent += new MyDel(this.WelcomeUser);
+        }
+        public string WelcomeUser(string username)
+        {
+            return "Welcome " + username;
+        }
+        static void Main(string[] args)
+        {
+            Event1 obj1 = new Event1();
+            string result = obj1.MyEvent("Ram");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Practice/Event2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class MyTest
+    {
+        public event EventHandler MyEvent
+        {
+            add
+            {
+                Console.WriteLine("add operation");
+            }
+            remove
+            {
+                Console.WriteLine("remove operation");
+            }
+        }
+    }
+    public class Event2
+    {
+        public void TestEvent()
+        {
+            MyTest myTest = new MyTest();
+            myTest.MyEvent += myTest_MyEvent;
+            myTest.MyEvent -= myTest_MyEvent;
+        }
+        public void myTest_MyEvent(object sender, EventArgs e)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            Event2 test = new Event2();
+            test.TestEvent();
+            Console.ReadKey();
+        }
+    }
+    
+}
